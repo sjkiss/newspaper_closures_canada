2cb66300777f4d1d8266a03550f4b91668e06d3e
Simon Kiss <sjkiss@gmail.com>
2022-05-04 12:31:29 -0400

    pushed up rename

--- R_Scripts/1_import_municipal_elections.R
+++ R_Scripts/1_import_municipal_elections.R
@@ -16,6 +16,7 @@ elections %>%
 table(elections$province, elections$election_year)
 
 
+#### Set up empty variables to be coded  ####
 #newspaper closures
 #This includes mergers that reduced the number of newspapers in a municipality by 1
 daily_newspaper_closed<-rep(0, nrow(elections))
@@ -25,8 +26,8 @@ community_weekly_newspaper_closed<-rep(0, nrow(elections))
 daily_newspaper_reduced_service<-rep(0, nrow(elections))
 community_weekly_newspaper_reduced_service<-rep(0, nrow(elections))
 
-#To ignore
-#university newspape
+# To ignore
+# university newspapers
 #Anything that opened and closed between elections (i.e. less than three years)
 #Anything in 
 # Montreal
@@ -46,26 +47,33 @@ community_weekly_newspaper_reduced_service<-rep(0, nrow(elections))
 # Frederiction
 
 
-##### Sample code for closure #### 
+#### Sample code for closure #### 
 #search for the municipality
 #Find the municipality you are looking for in two ways
 #Look for keywords and partial matches
 
 elections %>% 
-  filter(str_detect(municipality, "Moose"))
+  filter(str_detect(municipality, "Sainte-Marie")) %>% 
+  View()
 #Look for exact matches
 elections %>% 
-  filter(municipality=="Moose Jaw")
+  filter(municipality=="Rouyn-Noranda") %>% View()
 #
 #Assume a newspaper closed in Moose Jaw in 2010
 elections %>% 
-  mutate(newspaper_closed=case_when(
-    municipality=="Moose Jaw" & election_year > 2009 ~ 1
+  mutate(community_weekly_newspaper_closed=case_when(
+    municipality=="Sainte-Marie" & election_year > 2014 ~ 1,
+    municipality=="Rouyn-Noranda" & election_year > 2014 ~ 1,
+    municipality=="Selkirk" &election_year> 2020 ~1,
+    TRUE ~ 0
   ))->elections
 
+elections %>% 
+  filter(str_detect(municipality, "Sainte-Marie| Rouyn")) %>% 
+  View()
 #Provide a check
 elections %>% 
   filter(municipality=="Moose Jaw" & election_year> 2009) %>% 
   select(municipality, election_year, newspaper_closed)
 
-
+View(elections)
